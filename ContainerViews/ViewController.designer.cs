a31918471159d55873203777c3b22fe820fc51ec
richardwoollcott <richardwoollcott@hotmail.com>
2016-07-23 12:05:27 +0100

    Navigation buttons added

--- ContainerViews/ViewController.designer.cs
+++ ContainerViews/ViewController.designer.cs
@@ -16,13 +16,31 @@ namespace ContainerViews
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton FirstViewButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UIView MainContainer { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton SecondViewButton { get; set; }
 
         void ReleaseDesignerOutlets ()
         {
+            if (FirstViewButton != null) {
+                FirstViewButton.Dispose ();
+                FirstViewButton = null;
+            }
+
             if (MainContainer != null) {
                 MainContainer.Dispose ();
                 MainContainer = null;
+            }
+
+            if (SecondViewButton != null) {
+                SecondViewButton.Dispose ();
+                SecondViewButton = null;
             }
         }
     }
